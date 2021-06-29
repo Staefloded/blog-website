5d310ae35679f1c3e3053f9e24bb1030652aeb9c
staefloded <michaeleleyowo29@gmail.com>
2021-06-29 11:16:54 +0100

    Finished setting up website and creating layout component and deleting unnecessary files

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Header from "./Header";
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+      </Head>
+
+      <Header />
+      <main className="container mx-auto my-7">{children}</main>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Welcome to Dev space",
+  keywords: "development, coding, programming",
+  description: "The best info in news and development",
+};
---
+++ pages/blog/index.js
@@ -0,0 +1,5 @@
+import Layout from "../../components/Layout";
+
+export default function BlogPage() {
+  return <Layout>This is the blog page</Layout>;
+}
